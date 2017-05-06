af06f4647af1cb2aeecc59d5da4a2b66032a0e23
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-05-06 20:51:32 +0100

    news added

--- services/data.js
+++ services/data.js
@@ -122,7 +122,7 @@ module.exports = {
             news[key] = {}
             news[key].info = value.children[0].data
           })
-          $('tr span .spa').each((key, value) => {
+          $('tr .sp1').each((key, value) => {
             news[key].date = value.children[0].data
           })
           res.json(news)
